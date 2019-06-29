4ceb1653e5f1f82cd8033dbf1ddba0671a38c7c3
lori227 <lori227@qq.com>
2019-06-29 16:26:00 +0800

    Update display.lua

--- Content/Lua/Client/Logic/display.lua
+++ Content/Lua/Client/Logic/display.lua
@@ -6,14 +6,7 @@ function CDisplay:ctor()
 end
 
 function CDisplay:Init()
-    _message:Add( _protobuf:GetMsgId( "MSG_RESULT_DISPLAY" ), "KFMsg.MsgResultDisplay", 
-    function( msg ) 
-        self:HandleShowDisplay( msg ) 
-    end )
-end
-
-function CDisplay:HandleShowDisplay( msg )
-    self:ShowResult( msg.result, msg.param )
+    _message:Add( _protobuf:GetMsgId( "MSG_RESULT_DISPLAY" ), "KFMsg.MsgResultDisplay", function( msg ) self:ShowResult( msg.result, msg.param ) end )
 end
 
 function CDisplay:ShowResult( result, param )

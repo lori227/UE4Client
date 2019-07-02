e5a340b644d3d9caa9c21639f72dd69ceb31d286
lori227 <lori227@qq.com>
2019-07-02 18:58:59 +0800

    auth

--- Content/Lua/Client/Logic/Login/InternalAuth/internalauthui.lua
+++ Content/Lua/Client/Logic/Login/InternalAuth/internalauthui.lua
@@ -11,6 +11,7 @@ function CUIInternalAuth:OnCreate()
 
     self._button_auth = self._widget:FindWidget( 'ButtonAuth' )
     self._edit_account = self._widget:FindWidget( 'EditTextAccount' )
+    self._combo_url = self._widget:FindWidget( 'ComboAuthUrl' )
  
     self._button_auth.OnClicked:Add( function() self:OnClickButtonAuth() end )
 end
@@ -22,8 +23,19 @@ function CUIInternalAuth:OnShow()
 	if account ~= nil then
 		self._edit_account:SetText( account )
 	end
-end
 
+	self._combo_url:ClearOptions()
+	for _, v in pairs( AuthUrl ) do
+		self._combo_url:AddOption( v._name .. " | " .. v._url )
+	end
+
+	local selectindex = FLuaBind.ReadInt( "login", "url" )
+	if selectindex == nil or selectindex == 0 or selectindex > #AuthUrl then
+		selectindex = 1
+	end
+
+	self._combo_url:SetSelectedIndex( selectindex )
+end
 
 function CUIInternalAuth:OnClickButtonAuth()
 	local account = self._edit_account:GetText()
@@ -33,7 +45,16 @@ function CUIInternalAuth:OnClickButtonAuth()
 	end
 
 	FLuaBind.SaveString( "login", "account", account )
+	
+	local selectindex = self._combo_url:GetSelectedIndex()
+	if selectindex == 0 or selectindex == nil then
+		print( "select server auth url!")
+		return
+	end 
+	FLuaBind.SaveString( "login", "url", selectindex )
 
+	local url = AuthUrl[ selectindex ]._url
+	print( "account...".. account .."...url...".. url )
 	-- 发送验证消息
 
 end

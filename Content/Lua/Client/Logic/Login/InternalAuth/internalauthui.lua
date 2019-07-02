34fce1a13c7a873f4694ebe20468ffc0a9c88ffa
lori227 <lori227@qq.com>
2019-07-02 17:08:51 +0800

    Update internalauthui.lua

--- Content/Lua/Client/Logic/Login/InternalAuth/internalauthui.lua
+++ Content/Lua/Client/Logic/Login/InternalAuth/internalauthui.lua
@@ -7,7 +7,7 @@ end
 function CUIInternalAuth:OnCreate()
     CUIPanle.OnCreate( self )
 
-    self._widget = slua.loadUI('/Game/Blueprints/UI/Widget/Login/UI_InternalAuth.UI_InternalAuth')
+    self._widget = slua.loadUI( '/Game/Blueprints/UI/Widget/Login/UI_InternalAuth.UI_InternalAuth' )
 end
 
 return CUIInternalAuth
